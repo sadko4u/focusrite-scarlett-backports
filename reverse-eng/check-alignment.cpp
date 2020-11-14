610840131d93ec6008eba505027da377488d8eb2
sadko4u <sadko4u@gmail.com>
2020-11-14 23:57:21 +0300

    Updated description

--- reverse-eng/check-alignment.cpp
+++ reverse-eng/check-alignment.cpp
@@ -7,6 +7,7 @@
 
 #define SCARLETT2_ANALOGUE_IN_MAX          8    /* Maximum number of analogue inputs */
 #define SCARLETT2_ANALOGUE_OUT_MAX         10   /* Maximum number of analogue outputs */
+#define SCARLETT2_ALL_IN_MAX               42   /* Maximum number of all inputs */
 #define SCARLETT2_ALL_OUT_MAX              26   /* Maximum number of all outputs */
 #define SCARLETT2_LEVEL_SWITCH_MAX         2    /* Maximum number of LINE/INST switches */
 #define SCARLETT2_PAD_SWITCH_MAX           8    /* Maximum number of PAD switches */
@@ -50,14 +51,14 @@ struct scarlett2_sw_cfg {
     u8 __pad1[0x00bc];        /* +0x000c: ???????? */
     __le32 stereo_sw;         /* +0x01b4: stereo configuration for each port (bit mask) */
     __le32 mute_sw;           /* +0x01b8: mute switches (bit mask) */
-    struct scarlett2_sw_cfg_volume volumes[SCARLETT2_ANALOGUE_OUT_MAX]; /* +0x01bc: Volume settings of each output */
+    struct scarlett2_sw_cfg_volume volume[SCARLETT2_ANALOGUE_OUT_MAX];  /* +0x01bc: Volume settings of each output */
     u8 __pad2[0x01dc];        /* +0x01e4: ???????? */
-    u8 in_alias[SCARLETT2_ANALOGUE_IN_MAX][SCARLETT2_IN_NAME_LEN];      /* +0x03c0: Symbolic names of inputs */
-    u8 __pad3[0x05d0];        /* +0x0420: ???????? */
+    u8 in_alias[SCARLETT2_ALL_IN_MAX][SCARLETT2_IN_NAME_LEN];           /* +0x03c0: Symbolic names of inputs */
+    u8 __pad3[0x0438];        /* +0x0420: ???????? */
     u8 out_alias[SCARLETT2_ALL_OUT_MAX][SCARLETT2_OUT_NAME_LEN];        /* +0x09f0: Symbolic names of outputs */
-    u8 __pad4[0x05b4];        /* +0x0b28: ???????? */
-    __le32 mixer[SCARLETT2_SW_CONFIG_MIXER_OUTPUTS][SCARLETT2_SW_CONFIG_MIXER_INPUTS]; /* +0x10dc: Matrix mixer settings */
-    u8 __pad5[0x03f0];        /* +0x167c: ???????? */
+    u8 __pad4[0x04c8];        /* +0x0b28: ???????? */
+    __le32 mixer[SCARLETT2_SW_CONFIG_MIXER_OUTPUTS][SCARLETT2_SW_CONFIG_MIXER_INPUTS]; /* +0x0ff0: Matrix mixer settings */
+    u8 __pad5[0x04dc];        /* +0x1590: ???????? */
     __le32 checksum;          /* +0x1a6c: checksum of the area */
 } __packed;
 
@@ -77,7 +78,7 @@ int main(int argc, const char **argv)
     OFFSET(__pad1);
     OFFSET(stereo_sw);
     OFFSET(mute_sw);
-    OFFSET(volumes);
+    OFFSET(volume);
     OFFSET(__pad2);
     OFFSET(in_alias);
     OFFSET(__pad3);
